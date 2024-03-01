03f4448e8e81d3c42991b442bf526d01d07232b4
matteobolnre <matteo.bolner2@unibo.it>
2024-03-01 14:10:06 +0100

    Function to get records from ENA written and working as intended, tests needed

--- ena_tools/ena_records.py
+++ ena_tools/ena_records.py
@@ -2,7 +2,10 @@ import requests
 import pandas as pd
 import io
 
-def get_record_types(format:str='tsv') -> pd.DataFrame:
+def get_result_types(format:str='tsv') -> pd.DataFrame:
+    """
+    Get available result types (data sets) to search against.
+    """
     valid_formats = ['tsv', 'json']
     if format not in valid_formats:
         raise ValueError("Invalid format. Please choose one of: " + ', '.join(valid_formats))
@@ -24,14 +27,16 @@ def get_record_types(format:str='tsv') -> pd.DataFrame:
     else:
         response.raise_for_status()
 
-def get_fields_available_for_record_type(record_type: str, format:str='tsv' ) -> pd.DataFrame:
-    valid_record_types = ['analysis', 'analysis_study','assembly','coding','noncoding','read_experiment','read_run','read_study','sample','sequence','study','taxon']
-    invalid_record_types= ['tls_set','tsa_set','wgs_set']
-    if record_type not in valid_record_types:
-        if record_type in invalid_record_types:
-            raise ValueError(f"{record_type} is not accepted by the ENA api even if it is listed as result type \n Please choose one of: " + ', '.join(valid_record_types))
-        else:
-            raise ValueError("Invalid result type. Please choose one of: " + ', '.join(valid_record_types))
+
+def get_fields_available_for_result_type(result_type: str, format:str='tsv' ) -> pd.DataFrame:
+    """
+    Get fields that can be returned for a result type.
+    """
+
+    valid_result_types = ['analysis', 'analysis_study','assembly','coding','noncoding','read_experiment','read_run','read_study','sample','sequence','study','taxon']
+    invalid_result_types= ['tls_set','tsa_set','wgs_set']
+    if result_type not in valid_result_types:
+        raise ValueError("Invalid result type. Please choose one of: " + ', '.join(valid_result_types))
 
     headers = {
         'accept': '*/*',
@@ -39,7 +44,7 @@ def get_fields_available_for_record_type(record_type: str, format:str='tsv' ) ->
 
     params = {
         'dataPortal': 'ena', # doesnt matter which portal you choose, the results are the same
-        'result': record_type, #invalid result types are : 'tls_set','tsa_set','wgs_set'
+        'result': result_type, #invalid result types are : 'tls_set','tsa_set','wgs_set'
         'format': format,
     }
 
@@ -54,12 +59,78 @@ def get_fields_available_for_record_type(record_type: str, format:str='tsv' ) ->
         return response.raise_for_status()
 
 
-a=get_fields_available_for_record_type("sample")
+def get_accession_types_available_for_result_type(result_type: str) -> pd.DataFrame:
+    """
+    Get accession types that can be used in the search query.
+    """
+    valid_result_types = ['analysis', 'analysis_study','assembly','coding','noncoding','read_experiment','read_run','read_study','sample','sequence','study','taxon','tls_set','tsa_set','wgs_set']
+    if result_type not in valid_result_types:
+        raise ValueError("Invalid result type. Please choose one of: " + ', '.join(valid_result_types))
+
+    headers = {
+        'accept': '*/*',
+    }
+
+    params = {
+        'result': result_type,
+    }
+
+    response = requests.get('https://www.ebi.ac.uk/ena/portal/api/accessionTypes', params=params, headers=headers)
+    if response.ok:
+        accession_types=pd.DataFrame(response.json())['accessionType'].tolist()
+        return accession_types
+    else:
+        return response.raise_for_status()
 
 
+def ena_query(result_type:str, query:str, format:str='tsv',fields:str|list='all', limit=0,download=False,includeMetagenomes=False,dataPortal=None,includeAccessionType=[],includeAccessions=[],excludeAccessionType=[],excludeAccessions=[], dccDataOnly=False, rule=None):
+    if fields=='all':
+        fields=get_fields_available_for_result_type(result_type)['columnId'].tolist()
+
+    params={
+        'result':result_type,
+        'query':query,
+        'format':format,
+        'fields':'%2C'.join(fields),
+        'limit':limit,
+        'download': 'true' if download else 'false',
+        'includeMetagenomes':'true' if includeMetagenomes else 'false',
+        'dataPortal':dataPortal,
+        'includeAccessionType':'%2C'.join(includeAccessionType),
+        'includeAccessions':'%2C'.join(includeAccessions),
+        'excludeAccessions':'%2C'.join(excludeAccessions),
+        'excludeAccessionType':'%2C'.join(excludeAccessionType),
+        'dccDataOnly':'true' if dccDataOnly else 'false',
+        'rule':rule,
+    }
+
+    data=''
+    for param_name,param_value in params.items():
+        if param_value:
+            data+=f'{param_name}={param_value}&'
+        else:
+            data+=f'{param_name}=&'
+
+    data=data.rstrip("&")
+
+    headers = {
+        'accept': '*/*',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    response = requests.post('https://www.ebi.ac.uk/ena/portal/api/search', headers=headers, data=data)
+    if response.ok:
+        if format=='tsv':
+            records=pd.read_table(io.StringIO(response.text))
+        else:
+            records=pd.DataFrame(response.json())
+        return records
+    else:
+        return response.raise_for_status()
 
 
-#def get_ena_records(record_type, )
+a=ena_query(result_type='read_run', query='tax_tree(9822)', fields='all', limit=1000, format='tsv')
+
+
 
 
 '''
--- tests/ena_records.py
+++ tests/ena_records.py
@@ -2,26 +2,35 @@ import requests
 import pandas as pd
 import io
 import pytest
-from ena_tools.ena_records import get_record_types,get_fields_available_for_record_type
+from ena_tools.ena_records import get_result_types, get_fields_available_for_result_type, get_accession_types_available_for_result_type
 
 
-class TestGetEnaResultTypes:
+
+class TestGetEnaResulttypes:
     @pytest.mark.parametrize("format", ['tsv', 'json'])
 
     def test_valid_url(self, format):
-        result = get_record_types(format)
+        result = get_result_types(format)
 
         assert isinstance(result, pd.DataFrame)
         assert not result.empty
         assert len(result) >= 1
         assert list(result.columns)==['resultId', 'description', 'primaryAccessionType', 'recordCount', 'lastUpdated']
 
-class TestGetFieldsAvailableForResultType:
+class TestGetFieldsAvailableForResulttype:
     @pytest.mark.parametrize("format", ['tsv', 'json'])
-    @pytest.mark.parametrize("record_type", ['analysis', 'analysis_study','assembly','coding','noncoding','read_experiment','read_run','read_study','sample','sequence','study','taxon'])
-    def test_valid_url(self, record_type, format):
-        result = get_fields_available_for_record_type(record_type, format)
+    @pytest.mark.parametrize("result_type", ['analysis', 'analysis_study','assembly','coding','noncoding','read_experiment','read_run','read_study','sample','sequence','study','taxon'])
+    def test_valid_url(self, result_type, format):
+        result = get_fields_available_for_result_type(result_type, format)
         assert isinstance(result, pd.DataFrame)
         assert not result.empty
         assert len(result) >= 1
         assert list(result.columns)==['columnId', 'description', 'type']
+
+
+class TestGetAccessionTypesAvailableForResulttype:
+    @pytest.mark.parametrize("result_type", ['analysis', 'analysis_study','assembly','coding','noncoding','read_experiment','read_run','read_study','sample','sequence','study','taxon','tls_set','tsa_set','wgs_set'])
+    def test_valid_url(self, result_type):
+        result = get_accession_types_available_for_result_type(result_type)
+        assert isinstance(result, list)
+        assert len(result) >= 1
