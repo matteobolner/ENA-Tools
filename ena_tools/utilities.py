a31526935826ce6cb5a6727125811d3ee2fe2c0c
matteobolner <matteo.bolner2@unibo.it>
2024-06-25 17:21:50 +0200

    Cleaned up code

--- ena_tools/utilities.py
+++ ena_tools/utilities.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import unicodedata
+import re
 
-def split_df_in_rows(self, df):
+def split_df_in_rows(df):
     rows_list = [row for _, row in df.iterrows()]
     return rows_list
 
-def remove_diacritics(self, input_string):
+def remove_diacritics(input_string):
     input_string=str(input_string)
     return ''.join(
         char for char in unicodedata.normalize('NFD', input_string)
         if unicodedata.category(char) != 'Mn'  # Filter out combining characters
     )
 
-def strip_string_down_to_letters(self, input_string):
-    stripped_string=self.remove_diacritics(input_string)
+def strip_string_down_to_letters(input_string):
+    stripped_string=remove_diacritics(input_string)
     #stripped_string=re.sub(r'\W+', '', stripped_string)
     stripped_string = re.sub(r'[^a-zA-Z0-9\s]+', '', stripped_string)
     stripped_string=stripped_string.lower()
